76e22edf030d79f6c5fdffe364f835d9653b867c
soumya <mohapatrasoumya236@gmail.com>
2026-04-23 12:37:54 +0530

    remove duplicates code has been added

--- .ipynb_checkpoints/code-checkpoint.py
+++ .ipynb_checkpoints/code-checkpoint.py
@@ -9,3 +9,10 @@ if c == 1:
     print("prime")
 else:
     print("composite")
+-----------------------------------------
+x = [10,20,30,20,10,40]
+y = []
+for i in x:
+    if i not in y:
+        y.append(i)
+print(y)
